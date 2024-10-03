0459ec89a45a9d45cbaf450341d9845cdde12dc8
jagadish7-25 <kadapajag7@gmail.com>
2024-10-03 22:26:56 +0200

    subnets added

--- main.tf
+++ main.tf
@@ -4,5 +4,7 @@ module "vpc" {
     environment = var.environment 
     common_tags = var.common_tags
     public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
   
 }
