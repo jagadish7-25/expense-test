2d725e055f61ea2e9b41cdb0585770a12d64e5b1
jagadish7-25 <kadapajag7@gmail.com>
2024-10-03 22:17:01 +0200

    files modified

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ module "vpc" {
     project_name = var.project_name
     environment = var.environment 
     common_tags = var.common_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
   
 }
--- variables.tf
+++ variables.tf
@@ -15,3 +15,8 @@ variable "common_tags" {
 
   }
 }
+
+variable "public_subnet_cidrs" {
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
+  
+}
