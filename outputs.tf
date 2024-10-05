660007449277def3842eb33a0558949c3c58371f
jagadish7-25 <kadapajag7@gmail.com>
2024-10-05 13:11:02 +0200

    routes added

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@ output "vpc" {
     value = module.vpc.vpc_id
   
 }
+output "vpc_default" {
+    value = module.vpc.vpc_default
+  
+}
+output "main_route_table_info" {
+  value = module.vpc.main_route_table_info
+}
